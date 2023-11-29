87ba84a72de7471066e321473e5751f63eee6170
makeda banza <makedabanza16@gmail.com>
2023-11-29 17:50:16 +0200

    add contact to array

--- script/script.js
+++ script/script.js
@@ -1,34 +1,52 @@
-let prenom = document.querySelector('#prenom');
-let nom = document.querySelector('#nom');
-let tel = document.querySelector('#tel');
-let groupe = document.querySelector('#groupe');
-let bio = document.querySelector('#bio');
-let file= document.querySelector('#file');
-let btn_creer = document.querySelector('.create')
-let btn_reinit = document.querySelector('.reinit')
-let li = document.createElement('li')
-let ul = document.querySelector('ul')
+let prenom = document.querySelector("#prenom");
+let nom = document.querySelector("#nom");
+let tel = document.querySelector("#tel");
+let groupe = document.querySelector("#groupe");
+let bio = document.querySelector("#bio");
+let file = document.querySelector("#file");
+let email = document.querySelector("#email");
+let listContenair = document.querySelector(".contact-list");
+let btn_creer = document.querySelector(".create");
+let btn_reinit = document.querySelector(".reinit");
+let listContact = [];
 
-let formulaire_input=[prenom.value,nom.value,tel.value,groupe.value,bio.value] 
-btn_creer.addEventListener('click', function(){
-    let li = document.createElement('li')
-    li.innerHTML = prenom.value
-    li.innerHTML = nom.value
-    li.innerHTML = tel.value
-    li.innerHTML = groupe.value
-    li.innerHTML = bio.value
-    
-    ul.appendChild(li)
-})
+btn_reinit.addEventListener("click", function reinit(e) {
+  e.preventDefault();
+  if (confirm("voulez-vous réinitialiser tous les champs?") == true) {
+    prenom.value = "";
+    nom.value = "";
+    tel.value = "";
+    groupe.value = "";
+    bio.value = "";
+    file.value = "";
+    email.value = "";
+  }
+});
 
-btn_reinit.addEventListener('click', function reinit(e){
-    e.preventDefault();
-    if (confirm("voulez-vous réinitialiser tous les champs?") == true) {
-        prenom.value="";
-        nom.value="";
-        tel.value="";
-        groupe.value="";
-        bio.value="";
-        file.value=""
-      } 
-})
+// console.log(form);
+btn_creer.addEventListener("click", function (e) {
+  e.preventDefault();
+  let contact = new IdContact(
+    prenom.value,
+    nom.value,
+    tel.value,
+    groupe.value,
+    email.value,
+    bio.value,
+    file.value
+  );
+  listContact.push(contact);
+  showContact();
+  // console.log(listContact);
+});
+
+function IdContact(prenom, nom, telephone, groupe, email, bio, photo) {
+  this.prenom = prenom;
+  this.nom = nom;
+  this.telephone = telephone;
+  this.groupe = groupe;
+  this.email = email;
+  this.bio = bio;
+  this.photo = photo;
+}
+
