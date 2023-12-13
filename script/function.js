76c44a0297dcc38b94ec6155369dcdc920218912
makeda banza <makedabanza16@gmail.com>
2023-12-13 12:28:04 +0200

    hard reset

--- script/function.js
+++ script/function.js
@@ -22,26 +22,70 @@ function addContact() {
         bio.value,
         picture
     );
-    if(existeNumber){
-        msgNum.innerHTML='Ce numero existe dans le carnet'
-        tel.style.border="2px solid red"
-        tel.value=element.telephone
-        return false
-    }
-    
-    else if(existeEmail){
-        msgMail.innerHTML='Ce mail existe dans le carnet'
-        email.style.border="2px solid red"
-        email.value=element.telephone
-        return false
-    }
-    else if(!existeEmail || !existeNumber){
-        listContact.push(contact);
-        msgMail.innerHTML=""
-        email.style.border=""
-        msgNum.innerHTML=""
-        tel.style.border=""
-    }
+    if (!regex_nom.test(prenom.value)) {
+        msgPrenom.innerHTML = "le prenom doit avoir entre 3 et 50 caracteres";
+        prenom.style.border = "2px solid red";
+        prenom.value = element.prenom;
+        return false;
+      } else if (!regex_nom.test(nom.value)) {
+        msgNom.innerHTML = "le nom doit avoir entre 3 et 50 caracteres";
+        nom.style.border = "2px solid red";
+        nom.value = element.nom;
+        return false;
+      } else if (existeNumber) {
+        msgNum.innerHTML = "Ce numero existe dans le carnet";
+        tel.style.border = "2px solid red";
+        tel.value = element.telephone;
+        return false;
+      } else if (!regex_tel.test(tel.value)) {
+        msgNum.innerHTML = "le numéro est invalide";
+        tel.style.border = "2px solid red";
+        tel.value = element.telephone;
+        return false;
+      } else if (existeEmail) {
+        msgMail.innerHTML = "Ce mail existe dans le carnet";
+        email.style.border = "2px solid red";
+        tel.value = element.telephone;
+        email.value = element.email;
+        return false;
+      } else if (!regex_email.test(email.value)) {
+        msgMail.innerHTML = "Ce mail est invalide";
+        email.style.border = "2px solid red";
+        tel.value = element.telephone;
+        email.value = element.email;
+        return false;
+      } else if (picture == undefined) {
+        console.log("in undefined");
+        msgPhoto.innerHTML = "veillez charger une image";
+        div.style.border = "2px solid red";
+        return false;
+      } else {
+        let nomImage = file.files;
+        let nomImg = nomImage.length > 0 ? nomImage[0].name : "";
+        let estImage = regex_image.test(nomImg);
+        let tailleImg = nomImage.length > 0 ? taille[0].size : "";
+        if (!estImage) {
+          msgPhoto.innerHTML = "Le format de l'image nest pas prise en charge";
+          file.style.border = "2px solid red";
+          return false;
+        } else if (tailleImg > 5242880) {
+          msgPhoto.innerHTML = "l'image doit avoir moins de 5Mo";
+          div.style.border = "2px solid red";
+          return false;
+        } else {
+          listContact.push(contact);
+          msgMail.innerHTML = "";
+          email.style.border = "";
+          msgNum.innerHTML = "";
+          tel.style.border = "";
+          msgPhoto.innerHTML = "";
+          div.style.border = "";
+          msgNom.innerHTML = "";
+          nom.style.border = "";
+          msgPrenom.innerHTML = "";
+          prenom.style.border = "";
+        }
+      }
 }
 function editContact(i) {
     let contactModifie = new IdContact(
